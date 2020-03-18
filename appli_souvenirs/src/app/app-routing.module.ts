5fdad8d2edbc34b2019de0a980f5a73b32a7304f
Tom <pastor.tom.etu@gmail.com>
2020-03-18 14:50:24 +0100

    63000 fichiers c long

--- appli_souvenirs/src/app/app-routing.module.ts
+++ appli_souvenirs/src/app/app-routing.module.ts
@@ -3,9 +3,8 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},  {
-<<<<<<< HEAD
-<<<<<<< HEAD
+  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  {
     path: 'recherche-evenement',
     loadChildren: () => import('./recherche-evenement/recherche-evenement.module').then( m => m.RechercheEvenementPageModule)
   },
@@ -32,49 +31,4 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-const routes: Routes = [
-<<<<<<< HEAD
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
-  {
-    path: 'login',
-    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
-  },
-  {
-    path: 'register',
-    loadChildren: () => import('./register/register.module').then( m => m.RegisterPageModule)
-=======
-  { path: '', redirectTo: 'souvenirs', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
-  {
-<<<<<<< HEAD
-    path: 'souvenirs',
-    loadChildren: () => import('./souvenirs/souvenirs.module').then( m => m.SouvenirsPageModule)
->>>>>>> Juliane2
-  },
 
-=======
-    path: 'fil-souvenir',
-    loadChildren: () => import('./fil-souvenir/fil-souvenir.module').then( m => m.FilSouvenirPageModule)
-  },
-  {
-    path: 'rewind',
-    loadChildren: () => import('./rewind/rewind.module').then( m => m.RewindPageModule)
-  },
->>>>>>> manu
-];
-
-@NgModule({
-  imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
-  ],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
->>>>>>> Maxime
-=======
->>>>>>> testZineb
-=======
->>>>>>> Amine
